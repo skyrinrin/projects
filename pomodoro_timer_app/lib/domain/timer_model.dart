85943bc88416917cd5f571f3e60142317bddbd4c
skyrinrin <nneskygooogle@gmail.com>
2025-11-06 00:55:02 +0900

    'DurationとColorを変換し修正'

--- pomodoro_timer_app/lib/domain/timer_model.dart
+++ pomodoro_timer_app/lib/domain/timer_model.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class TimerModel {
   final Duration mainTime;
   final Duration subTime;
-  final Color color;
+  final int color;
   // final Duration remaining;
   // final bool isRunning;
   // final DateTime? targetEnd; //nullなら停止中
@@ -13,39 +13,47 @@ class TimerModel {
   TimerModel({
     required this.mainTime,
     required this.subTime,
-    required this.color,
+    required Color color,
     // required this.remaining,
     // required this.isRunning,
     // required this.targetEnd,
-  });
+  }) : color = color.value;
 
-  TimerModel copyWith({
-    Duration? mainTime,
-    Duration? subTime,
-    Color? color,
-    // Duration? remaining,
-    // bool? isRunning,
-    // DateTime? targetEnd,
-  }) => TimerModel(
-    mainTime: mainTime ?? this.mainTime,
-    subTime: subTime ?? this.subTime,
-    color: color ?? this.color,
-    // remaining: remaining ?? this.remaining,
-    // isRunning: isRunning ?? this.isRunning,
-    // targetEnd: targetEnd ?? this.targetEnd,
-  );
+  //使う時に復元
+  // TimerModel copyWith({
+  //   Duration? mainTime,
+  //   Duration? subTime,
+  //   Color? color,
+  //   // Duration? remaining,
+  //   // bool? isRunning,
+  //   // DateTime? targetEnd,
+  // }) => TimerModel(
+  //   mainTime: mainTime ?? this.mainTime,
+  //   subTime: subTime ?? this.subTime,
+  //   color: color ?? this.color,
+  //   // remaining: remaining ?? this.remaining,
+  //   // isRunning: isRunning ?? this.isRunning,
+  //   // targetEnd: targetEnd ?? this.targetEnd,
+  // );
 
   /// JSONに変換（保存用）
   Map<String, dynamic> toJson() {
-    return {'mainTime': mainTime, 'subTime': subTime, 'color': color};
+    return {
+      'mainTime': mainTime.inSeconds,
+      'subTime': subTime.inSeconds,
+      'color': color,
+    };
   }
 
   /// JSONから復元（読み込み用）
   factory TimerModel.fromJson(Map<String, dynamic> json) {
     return TimerModel(
-      mainTime: json['mainTime'] ?? 1500,
-      subTime: json['subTime'] ?? 300,
-      color: json['color'] ?? Color(0xFFB7E9FF),
+      // mainTime: json['mainTime'] ?? 1500,
+      // subTime: json['subTime'] ?? 300,
+      // color: json['color'] ?? Color(0xFFB7E9FF),
+      mainTime: Duration(seconds: json['mainTime']),
+      subTime: Duration(seconds: json['subTime']),
+      color: Color(json['color']),
     );
   }
 
